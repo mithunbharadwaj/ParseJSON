2ceddc96cb3e4867cc1dbfbe68e637b197381ec3
mithunbharadwaj <mithunshivashankar@gmail.com>
2020-08-28 11:55:42 +0200

    Refactored the get_stats function Printing output records for debugging Code formatting Comments

--- main.py
+++ main.py
@@ -2,110 +2,107 @@ import json
 import socket
 import mysql.connector
 
+
+# Get performance statistics - cpu, memory, addresses
+def get_stats(y):
+    # Returned crate, initialized to defaults
+    ret_crate = {'cpu': "N/A", 'memory_usage': "N/A", 'ip_addresses': []}
+
+    # Check if there is a state, if not, return an empty crate
+    state = y['state']
+    if state is None:
+        return ret_crate
+
+
+    # Check for memory usage
+    memory = state['memory']
+    if memory is not None:
+        mem_usage = memory['usage']
+        if mem_usage is not None:
+            ret_crate['memory_usage'] = mem_usage
+
+    # Get lists of IP addresses
+    network = state['network']
+    if network is None:
+        pass
+    else:
+        for network_instance in network:
+            instance = network[network_instance]
+            addresses = instance['addresses']
+            for single_address in addresses:
+                ip = single_address['address']
+                # print(ip)
+                ret_crate['ip_addresses'].append(ip)
+
+    # Get CPU usage if available
+    cpu = state['cpu']
+    if cpu is not None:
+        usage = cpu['usage']
+        if usage is not None:
+            ret_crate['cpu']=usage
+
+    #Return the final crate
+    return ret_crate
+
+def get_status(y):
+    status = y['status']
+    if status is None:
+        return "N/A"
+    else:
+        return status
+
+
+def get_creation_time(y):
+    created_at = y['created_at']
+    if created_at is None:
+        return "N/A"
+    else:
+        return created_at
+
+
+def get_name(y):
+    name = y['name']
+    if name is None:
+        return "N/A"
+    else:
+        return name
+
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Mainline code
+# -----------------------------------------------------------------------------------------------------------------------
+
+# Open the JSON file and parse it with standard library
+
 with open('/Users/MITHUN/PycharmProjects/ParseJSON/sample-data.json') as access_json:
-    x = json.load(access_json)
-    sql_stm = []
-    sql_dict = {}
-
-#comment come mf
-def getCPU(y):
-        state = y['state']
-        if not (state is None):
-            state = y['state']
-
-            memory = state['memory']
-            mem_usage = memory['usage']
-
-            ip_List4 = []
-            ip_List6 = []
-
-            network = state['network']
-            for network_instance in network:
-                instance = network[network_instance]
-                addresses = instance['addresses']
-                for single_address in addresses:
-                    ip = single_address['address']
-                    family = single_address['family']
-                    try:
-                        socket.inet_pton(socket.AF_INET,ip)
-                        ip_List4.append(ip)
-                    except:
-                        pass
-
-                    try:
-                        socket.inet_pton(socket.AF_INET6,ip)
-                        ip_List6.append(ip)
-                    except:
-                        pass
-
-            cpu = state['cpu']
-            usage = cpu['usage']
-
-            sql_stm.append(usage)
-            sql_dict['memory'] = mem_usage
-            sql_dict['ipList4'] = ip_List4
-            sql_dict['ipList6'] = ip_List6
-            sql_dict['cpu'] = usage;
-        else:
-            sql_stm.append('N/A')
-            sql_dict['cpu'] = 'N/A'
-
-def getStatus(y):
-        status = y['status']
-        if not (status is None):
-            sql_stm.append(status)
-            sql_dict['status'] = status;
+    input_json = json.load(access_json)
 
 
-def getCreated_at(y):
-    created_att = y['created_at']
-    sql_stm.append(created_att)
-    sql_dict['created'] = created_att;
-    #print(sql_stm)
+output_records = []
+
+for record in input_json:
+    status = get_status(record)
+    name = get_name(record)
+    cpu = get_stats(record)
+    created_at = get_creation_time(record)
 
 
-def getName(y):
-    getTheName = y['name']
-    sql_stm.append(getTheName)
-    sql_dict['name'] = getTheName
+    # Create final output record
+    output_record = {}
 
-#def getIpaddress():
+    output_record['status']=status
+    output_record['name']=name
+    output_record['created_at']=created_at
 
+    output_record['cpu']=cpu['cpu']
+    output_record['memory_usage']=cpu['memory_usage']
+    output_record['ip_addresses']=cpu['ip_addresses']
 
+    output_records.append(output_record)
 
-SQLDict = []
-SQLList = []
-for y in x:
-    getStatus(y)
-    getName(y)
-    getCPU(y)
-    getCreated_at(y)
-    SQLList.append(sql_stm)
-    SQLDict.append(sql_dict)
-    sql_stm = []
-    sql_dict = {}
-
-for one_sql in SQLDict:
-    print(one_sql)
-
-
-
-
-"""
-for y in range(16):
-    getStatus(x[y])
-    getName(x[y])
-    getCPU(x[y])
-    getCreated_at(x[y])
-    print(sql_stm)
-"""
-
-
-
-
-
-
-
-
-
-
+#Print to the standard output for debugging and diagnostics
+count=0
+for one_record in output_records:
+    print(str(count)+": " +str(one_record))
+    count=count+1
