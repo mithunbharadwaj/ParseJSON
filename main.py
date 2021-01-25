90757214ccac230ecc26cbabeedd103170da5623
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-01-25 22:20:17 +0100

    Add comments

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
+#read the CSV
 data = pd.read_csv("Qualitative_Bankruptcy.data.txt")
 
+#Name columns
 data.columns = ["Industrial Risk","Industrial Risk"," Financial Flexibility","Credibility","Competitiveness","Operating Risk","Class"]
 
+#store file as data.csv
 data.to_csv("data.csv",index=None,header=True)
